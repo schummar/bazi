986c9dbb83e61bf7563a7e82a1e63004a39c21f3
Marco Schumacher <marco@schummar.de>
2014-03-17 18:43:01 +0100

    add comments

--- common/src/main/java/api/de/uni_augsburg/bazi/common/algorithm/Algorithm.java
+++ common/src/main/java/api/de/uni_augsburg/bazi/common/algorithm/Algorithm.java
@@ -7,12 +7,31 @@ import de.uni_augsburg.bazi.common.data.Data;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Basically a Data->Data function. Calling {@link #apply(Data, Options)}
+ * automatically uses available Filters for pre- and postprocessing.
+ */
 public interface Algorithm<O extends Data> extends Plugin.Instance
 {
+	/** @return a list of Attributes this algorithm uses in its input data */
 	List<Object> getInputAttributes();
 
+
+	/**
+	 * Applies this algorithm to the given input without using any filters.
+	 * @param in input
+	 * @param options general calculation options
+	 * @return the algorithm's result
+	 */
 	O applyUnfiltered(Data in, Options options);
 
+
+	/**
+	 * Applies this algorithm to the given input. Automatically uses available Filters for pre- and postprocessing.
+	 * @param in input
+	 * @param options general calculation options
+	 * @return the algorithm's result
+	 */
 	default O apply(Data in, Options options)
 	{
 		List<Filter> filters = PluginManager.INSTANCE.getFiltersFor(this);
