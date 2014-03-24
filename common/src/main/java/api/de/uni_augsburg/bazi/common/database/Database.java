2fb39e0ee3dbe15e8ae13a9b0c7d1aa5d651fc2e
Marco Schumacher <marco@schummar.de>
2014-03-24 11:39:59 +0100

    add comments to common.database

--- common/src/main/java/api/de/uni_augsburg/bazi/common/database/Database.java
+++ common/src/main/java/api/de/uni_augsburg/bazi/common/database/Database.java
@@ -2,5 +2,6 @@ package de.uni_augsburg.bazi.common.database;
 
 import de.uni_augsburg.bazi.common.Plugin;
 
+/** Interface of a bazi database.Not specified yet. */
 public interface Database extends Plugin.Instance
 {}
